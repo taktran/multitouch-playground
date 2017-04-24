2380b4e90cb9127608c8747a276a1ef65808b22c
Tak Tran <contact@tutaktran.com>
2017-04-24 22:45:10 +0100

    Show distance values

--- src/components/Playground/playground.js
+++ src/components/Playground/playground.js
@@ -6,9 +6,10 @@ const Game = ({
 }) => {
   if (!Phaser) { throw new Error('Phaser is required'); }
 
-  const state = {};
+  const state = {
+    debug: true
+  };
   const game = new Phaser.Game(width, height, Phaser.AUTO, '', {
-    preload: preload,
     create: create,
     update: update,
     render: render
@@ -20,20 +21,30 @@ const Game = ({
 
   const showPointers = () => {
     game.debug.pointer(game.input.mousePointer);
-    game.debug.pointer(game.input.pointer1);
-    game.debug.pointer(game.input.pointer2);
-    game.debug.pointer(game.input.pointer3);
-    game.debug.pointer(game.input.pointer4);
-    game.debug.pointer(game.input.pointer5);
-    game.debug.pointer(game.input.pointer6);
-    game.debug.pointer(game.input.pointer7);
-    game.debug.pointer(game.input.pointer8);
-    game.debug.pointer(game.input.pointer9);
-    game.debug.pointer(game.input.pointer10);
-  }
 
-  function preload() {
+    const {
+      pointer1,
+      pointer2,
+      pointer3,
+      pointer4,
+      pointer5,
+      pointer6,
+      pointer7,
+      pointer8,
+      pointer9,
+      pointer10
+    } = game.input;
 
+    game.debug.pointer(pointer1);
+    game.debug.pointer(pointer2);
+    game.debug.pointer(pointer3);
+    game.debug.pointer(pointer4);
+    game.debug.pointer(pointer5);
+    game.debug.pointer(pointer6);
+    game.debug.pointer(pointer7);
+    game.debug.pointer(pointer8);
+    game.debug.pointer(pointer9);
+    game.debug.pointer(pointer10);
   }
 
   // let emitter;
@@ -89,8 +100,24 @@ const Game = ({
   //   });
   // }
 
-  function update() {
 
+  let dist1to2 = 0;
+  let dist1to3 = 0;
+  let dist1to4 = 0;
+
+  function update() {
+    const {
+      pointer1,
+      pointer2,
+      pointer3,
+      pointer4
+    } = game.input;
+
+    if (pointer1.active) {
+      dist1to2 = pointer2.active ? pointer1.position.distance(pointer2.position) : 0;
+      dist1to3 = pointer3.active ? pointer1.position.distance(pointer3.position) : 0;
+      dist1to4 = pointer4.active ? pointer1.position.distance(pointer4.position) : 0;
+    }
   }
 
   function render() {
@@ -98,6 +125,10 @@ const Game = ({
 
     if (debug) {
       showPointers();
+
+      game.debug.text(`p1 -> p2: ${dist1to2}`, 32, 32);
+      game.debug.text(`p1 -> p3: ${dist1to3}`, 32, 64);
+      game.debug.text(`p1 -> p4: ${dist1to4}`, 32, 96);
     }
   }
 
